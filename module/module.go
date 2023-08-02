4e93f38a929aeb93eaad2b0b57740b4c02051d70
Ahmetcan ÖZCAN <ahmetcanozcan7@gmail.com>
2023-08-02 21:56:09 +0300

    Add functional modules

--- module/module.go
+++ module/module.go
@@ -4,3 +4,15 @@ type Module interface {
 	Require() (any, error)
 	Copy() Module
 }
+
+var _ Module = (FuncModule)(nil)
+
+type FuncModule func() (any, error)
+
+func (fm FuncModule) Require() (any, error) {
+	return fm()
+}
+
+func (fm FuncModule) Copy() Module {
+	return fm
+}
